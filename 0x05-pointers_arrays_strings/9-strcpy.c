647fbe23eb02e1e97447f73a971f248c4521116a
ramadanict <ramadanhundessa@gmail.com>
2022-12-20 13:45:59 -0800

    9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,8 +11,8 @@ char *_strcpy(char *dest, char *src)
 {
 int i;
 for (i = 0; src[i] != '\0'; i++)
-dest[i] = src[];
+dest[i] = src[i];
 for ( ; i < src[i]; i++)
-dest[i] = '\0';
+dest[i] = '0';
 return (dest);
 }
