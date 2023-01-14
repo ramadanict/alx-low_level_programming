0a7ef722ac87c5e6891d96f814943657ea0b9a71
ramadanict <ramadanhundessa@gmail.com>
2023-01-14 01:36:42 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,53 +1,25 @@
 #include "main.h"
-
 /**
-
- * rot13 - encoding string into rot13 format
-
- * @s: char passed for conversion
-
- * Return: char
-
- */
-
+* rot13 - encoding string into rot13 format
+* @s: char passed for conversion
+* Return: char
+*/
 char *rot13(char *s)
-  
 {
-  
-  int j;
-  
-  int i = 0;
-  
-  char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  
-  char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-  
-
-  
-  while (s[i] != '\0')
+int j;
+int i = 0;
+char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+while (s[i] != '\0')
+{
+j = 0;
+while (alpha[j] != '\0')
+{
+if (s[i] == alpha[j])
+{
+s[i] = rot[j];
+break;
     
-    {
-      
-      j = 0;
-      
-      while (alpha[j] != '\0')
-	
-	{
-	  
-	  if (s[i] == alpha[j])
-	    
-	    {
-	      
-	      s[i] = rot[j];
-	      
-	      break;
-	      
-	    }
-	  
-	  j++;
-	  
-	}
-      
       i++;
       
     }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,44 +1,18 @@
 #include "main.h"
-
-
-
 /**
-
- * *string_toupper - changes all lowercase letters of a string to uppercase.
-
- * @a:  string
-
- * Return: string
-
- **/
-
-
-
+* *string_toupper - changes all lowercase letters of a string to uppercase.
+* @a:  string
+* Return: string
+**/
 char *string_toupper(char *a)
-  
 {
-  
-  int i;
-  
-
-  
-  i = 0;
-  
-
-  
-  while (a[i] != '\0')
-    
-    {
-      
-      if (a[i] >= 'a' && a[i] <= 'z')
-	
-	a[i] = a[i] - 32;
-      
-      i++;
-      
-    }
-  
-  return (a);
-  
+int i;
+i = 0;
+while (a[i] != '\0')
+{
+if (a[i] >= 'a' && a[i] <= 'z')
+a[i] = a[i] - 32;
+i++;
 }
-
+return (a);
+}
