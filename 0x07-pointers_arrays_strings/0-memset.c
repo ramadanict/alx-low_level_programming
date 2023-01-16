7546073128a8277a1792cb15691d6d3cd66292b0
ramadanict <ramadanhundessa@gmail.com>
2023-01-15 23:52:14 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _memset - entry point
+* Description: sets the memory of the bytes.
+* @b: n-val.
+* @s: f-byte.
+* @n: c-bytes.
+* Return: Pointer s.
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int mem;
+for (mem = 1; mem <= n; mem++)
+*(s + (mem - 1)) = b;
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+**_strchr - entry point
+* Description: locates a character in a string
+*@s: pointer with string
+*@c: char
+*Return: s
+*/
+char *_strchr(char *s, char c)
+{
+int lchar;
+for (lchar = 0; s[0] != '\0'; lchar++)
+{
+if (s[0] == c)
+{
+return (s);
+}
+s++;
+}
+if (s[0] == c)
+{
+return (s);
+}
+return ('\0');
+}
