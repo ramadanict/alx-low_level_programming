41385e6568dcc49bf9ddd128c2092532251313ab
ramadanict <ramadanhundessa@gmail.com>
2023-01-16 09:20:57 -0800

    2-largest_number.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,6 +3,7 @@
 /**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
+* Description: prints the date
 * @month: month in number format
 * @day: day of month
 * @year: year
@@ -10,7 +11,7 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
+if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
 if (month > 2 && day >= 60)
 {
